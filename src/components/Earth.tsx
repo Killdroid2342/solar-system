5db5e6bc988d9ac24a271d9c1c48d78510a9039e
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-26 00:23:09 +0100

    added new planets

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,16 +1,17 @@
+import React from 'react';
 import { useRef } from 'react';
 import { Mesh } from 'three';
 import { OrbitControls } from '@react-three/drei';
 import { useTexture } from '@react-three/drei';
-
-export function Sun() {
-  const texture = useTexture('src/assets/img/sun.jpg');
+import { useFrame } from '@react-three/fiber';
+export function Earth() {
   const systemRef = useRef<Mesh>(null!);
-
+  const texture = useTexture('src/assets/img/earth.jpg');
+  useFrame(() => {});
   return (
     <>
-      <mesh ref={systemRef}>
-        <sphereGeometry args={[5, 128, 64]} />
+      <mesh ref={systemRef} position={[60, 0, 0]}>
+        <sphereGeometry args={[0.9, 128, 64]} />
         <meshStandardMaterial map={texture} />
         <OrbitControls />
       </mesh>
--- src/components/Venus.tsx
+++ src/components/Venus.tsx
@@ -1,16 +1,17 @@
+import React from 'react';
 import { useRef } from 'react';
 import { Mesh } from 'three';
 import { OrbitControls } from '@react-three/drei';
 import { useTexture } from '@react-three/drei';
-
-export function Sun() {
-  const texture = useTexture('src/assets/img/sun.jpg');
+import { useFrame } from '@react-three/fiber';
+export function Venus() {
   const systemRef = useRef<Mesh>(null!);
-
+  const texture = useTexture('src/assets/img/venus.jpg');
+  useFrame(() => {});
   return (
     <>
-      <mesh ref={systemRef}>
-        <sphereGeometry args={[5, 128, 64]} />
+      <mesh ref={systemRef} position={[40, 0, 0]}>
+        <sphereGeometry args={[0.8, 128, 64]} />
         <meshStandardMaterial map={texture} />
         <OrbitControls />
       </mesh>
