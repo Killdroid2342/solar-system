edc3ac3f1bc8790a852b5705e7988857cd2730be
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-27 05:31:15 +0100

    fixed lighting

--- src/components/Sun.tsx
+++ src/components/Sun.tsx
@@ -1,17 +1,18 @@
 import { useRef } from 'react';
-import { Mesh } from 'three';
-import { OrbitControls } from '@react-three/drei';
-import { useTexture } from '@react-three/drei';
+import { Mesh, PointLight, PointLightHelper } from 'three';
+import { OrbitControls, useHelper, useTexture } from '@react-three/drei';
 
 export function Sun() {
   const texture = useTexture('src/assets/img/sun.jpg');
   const systemRef = useRef<Mesh>(null!);
-
+  const pointLightRef = useRef<PointLight>(null!);
+  // useHelper(pointLightRef, PointLightHelper, 180, 'green');
   return (
     <>
       <mesh ref={systemRef}>
         <sphereGeometry args={[5, 128, 64]} />
         <meshStandardMaterial map={texture} />
+        <pointLight ref={pointLightRef} intensity={20000} />
         <OrbitControls />
       </mesh>
     </>
