612c9c837a037fd3a089e2dfda34f39026e8a673
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-29 06:10:44 +0100

    added glow and fixed loading issue

--- src/components/planets/Sun.tsx
+++ src/components/planets/Sun.tsx
@@ -1,29 +1,27 @@
 import { useRef } from 'react';
 import { Mesh, PointLight, PointLightHelper } from 'three';
 import { OrbitControls, useHelper, useTexture } from '@react-three/drei';
-import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
 export function Sun() {
   const texture = useTexture('src/assets/img/sun.jpg');
   const systemRef = useRef<Mesh>(null!);
   const pointLightRef = useRef<PointLight>(null!);
-  // useHelper(pointLightRef, PointLightHelper, 180, 'green');
+  useHelper(pointLightRef, PointLightHelper, 260, 'green');
+
   return (
     <>
       <mesh ref={systemRef}>
         <sphereGeometry args={[5, 128, 64]} />
         <meshStandardMaterial map={texture} />
-        <pointLight ref={pointLightRef} intensity={20000} />
+        <meshPhysicalMaterial
+          roughness={0.2}
+          metalness={0.5}
+          emissiveMap={texture}
+          emissive={'white'}
+          emissiveIntensity={1}
+        />
+        <pointLight ref={pointLightRef} intensity={9000} />
         <OrbitControls />
-        {/* <EffectComposer multisampling={8}>
-          <Bloom
-            kernelSize={3}
-            luminanceThreshold={0}
-            luminanceSmoothing={0.4}
-            intensity={0.6}
-          />
-        </EffectComposer> */}
-        <Sun />
       </mesh>
     </>
   );
