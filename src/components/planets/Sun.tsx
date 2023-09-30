0eb0a84866c1dc7cf8f08b375dcdf0417f18e536
Haroon Khan <HaroonKhan120704@gmail.com>
2023-09-30 08:59:17 +0100

    removed helper

--- src/components/planets/Sun.tsx
+++ src/components/planets/Sun.tsx
@@ -1,12 +1,11 @@
 import { useRef } from 'react';
-import { Mesh, PointLight, PointLightHelper } from 'three';
-import { OrbitControls, useHelper, useTexture } from '@react-three/drei';
+import { Mesh, PointLight } from 'three';
+import { OrbitControls, useTexture } from '@react-three/drei';
 
 export function Sun() {
   const texture = useTexture('src/assets/img/sun.jpg');
   const systemRef = useRef<Mesh>(null!);
   const pointLightRef = useRef<PointLight>(null!);
-  useHelper(pointLightRef, PointLightHelper, 260, 'green');
 
   return (
     <>
