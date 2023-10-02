86daa6100d6255462149d5d81f0fa3e35b3cb56f
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-03 00:18:36 +0100

    implemented data for planets

--- src/components/planets/PlanetModal/Modal.tsx
+++ src/components/planets/PlanetModal/Modal.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 
-const Modal = ({ closeModal }: any) => {
+const Modal = ({ closeModal, selectedPlanet, data }: any) => {
+  console.log(data);
   return (
-    <>
-      <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
-        <div className='bg-transparent p-4 rounded-lg overflow-auto max-h-96 text-white border border-white'>
-          <button
-            className='mt-2 px-4 py-2 border border-white text-white rounded-lg'
-            onClick={closeModal}
-          >
-            X
-          </button>
-          <h2 className='text-2xl font-semibold mb-4'>{`Information About NAME OF ${'PLANET GOES HERE'}`}</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-            mollitia ad numquam! Ut esse non, reiciendis iure consectetur fugiat
-            inventore aut ipsam, vel repellat suscipit corporis ipsum
-            exercitationem tempora sunt. FACTS HERE
-          </p>
-        </div>
+    <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
+      <div className='bg-transparent p-4 rounded-lg overflow-auto max-h-96 text-white border border-white'>
+        <button
+          className='mt-2 px-4 py-2 border border-white text-white rounded-lg'
+          onClick={closeModal}
+        >
+          X
+        </button>
+        <h2 className='text-2xl font-semibold mb-4'>{`Information About ${selectedPlanet}`}</h2>
+        {data?.map((planet: any) => {
+          if (planet.name === selectedPlanet) {
+            return (
+              <div key={planet.planetOrder}>
+                <p className='text-white text-center text-2xl'>{planet.name}</p>
+                <p className='text-white text-center'>{planet.description}</p>
+                <p className='text-white text-center'>
+                  {` Mass of ${planet.name} ${' '} ${planet.basicDetails.mass}`}
+                </p>
+
+                <p className='text-white text-center'>
+                  {` Volume of ${planet.name} ${' '} ${
+                    planet.basicDetails.volume
+                  }`}
+                </p>
+                <p className='text-white text-center'>{`Order of ${
+                  planet.name
+                }${' '}${planet.planetOrder}`}</p>
+              </div>
+            );
+          }
+        })}
       </div>
-    </>
+    </div>
   );
 };
 
