acfef741bd95bcabecdd3ed11b4d3c616796fb3a
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-05 06:31:41 +0100

    added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,11 @@ import { useState, useEffect } from 'react';
 import Modal from './components/planets/PlanetModal/Modal';
 const sunData = {
   name: 'Sun',
-  description: 'dfgdfgdfgd',
-  mass: 12983,
-  volume: 12132123132,
-  planetOrder: 'asdasd',
+  description:
+    'The Sun is a 4.5 billion-year-old yellow dwarf star – a hot glowing ball of hydrogen and helium – at the center of our solar system. It’s about 93 million miles (150 million kilometers) from Earth and it’s our solar system’s only star. Without the Sun’s energy, life as we know it could not exist on our home planet.',
+  mass: '1.989 × 10^30 kg',
+  volume: '1,412,000,000,000 km^3',
+  planetOrder: '0',
 };
 console.log(sunData);
 const App = () => {
@@ -53,7 +54,7 @@ const App = () => {
     getData();
   }, []);
   return (
-    <div className='h-screen'>
+    <div className='h-screen cursor-grab active:cursor-grabbing'>
       <Canvas>
         <PerspectiveCamera makeDefault fov={50} position={[90, 120, 90]} />
         <color attach='background' args={['black']} />
