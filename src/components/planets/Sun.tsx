3632b13b67bfe39df33150fc2e10ab881ee34548
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-06 09:09:23 +0100

    adding back bloom

--- src/components/planets/Sun.tsx
+++ src/components/planets/Sun.tsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import { Mesh, PointLight } from 'three';
 import { OrbitControls, useTexture } from '@react-three/drei';
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
+import { KernelSize, Resolution } from 'postprocessing';
 
 export function Sun({ OpenModal }: any) {
   const texture = useTexture('src/assets/img/sun.jpg');
@@ -11,16 +13,18 @@ export function Sun({ OpenModal }: any) {
     <>
       <mesh ref={systemRef} onClick={OpenModal}>
         <sphereGeometry args={[5, 128, 64]} />
-        <meshStandardMaterial map={texture} />
         <meshPhysicalMaterial
           roughness={0.2}
           metalness={0.5}
           emissiveMap={texture}
           emissive={'white'}
-          emissiveIntensity={1}
+          emissiveIntensity={2}
         />
         <pointLight ref={pointLightRef} intensity={9000} />
         <OrbitControls />
+        <EffectComposer>
+          <Bloom />
+        </EffectComposer>
       </mesh>
     </>
   );
