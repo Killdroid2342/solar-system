f816d6e5e160605d6038e270ba092e2871f25798
Haroon Khan <HaroonKhan120704@gmail.com>
2023-10-09 07:39:07 +0100

    cleaned modal

--- src/components/planets/PlanetModal/Modal.tsx
+++ src/components/planets/PlanetModal/Modal.tsx
@@ -1,49 +1,34 @@
 const Modal = ({ closeModal, selectedPlanet, data, sunData }: any) => {
-  const planetData = data.find((planet: any) => planet.name === selectedPlanet);
-
+  let volume = data.basicDetails?.volume || sunData.volume;
+  let mass = data.basicDetails?.mass || sunData.mass;
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black'>
       <div className='bg-transparent p-4 rounded-lg overflow-auto max-h-96 text-white border border-white'>
         <button
           className='mt-2 px-4 py-2 border border-white text-white rounded-lg'
-          onClick={closeModal}
+          onClick={() => {
+            closeModal();
+          }}
         >
           X
         </button>
-
-        {planetData ? (
-          <div>
-            <h2 className='text-3xl font-semibold mb-4 text-center'>{`Information About ${selectedPlanet}`}</h2>
-            <p className='text-white'>{planetData.description}</p>
-            <p className='text-white mt-5'>
-              {`Mass of ${planetData.name}${': '} ${
-                planetData.basicDetails.mass
-              }`}
-            </p>
-            <p className='text-white  mt-5'>
-              {`Volume of ${planetData.name}${': '} ${
-                planetData.basicDetails.volume
-              }`}
-            </p>
-            <p className='text-white  mt-5'>{`Order of ${
-              planetData.name
-            }${': '}${planetData.planetOrder}`}</p>
-          </div>
-        ) : (
-          <div>
-            <h2 className='text-3xl font-semibold mb-4 text-center'>{`Information About ${sunData.name}`}</h2>
-            <p className='text-white'>{sunData.description}</p>
-            <p className='text-white mt-5'>
-              {`Mass of ${sunData.name}${': '} ${sunData.mass}`}
-            </p>
-            <p className='text-white mt-5'>
-              {`Volume of ${sunData.name}${': '} ${sunData.volume}`}
-            </p>
-            <p className='text-white mt-5'>{`Order of ${sunData.name}${': '}${
-              sunData.planetOrder
-            }`}</p>
-          </div>
-        )}
+        <div>
+          <h2 className='text-3xl font-semibold mb-4 text-center'>{`Information About ${
+            selectedPlanet || sunData.name
+          }`}</h2>
+          <p className='text-white'>
+            {data.description || sunData.description}
+          </p>
+          <p className='text-white mt-5'>
+            {`Mass of ${data.name}${': '} ${mass}`}
+          </p>
+          <p className='text-white mt-5'>
+            {`Volume of ${data.name || sunData.name}${': '} ${volume}`}
+          </p>
+          <p className='text-white  mt-5'>{`Order of ${
+            data.name || sunData.name
+          }${': '}${data.planetOrder || sunData.planetOrder}`}</p>
+        </div>
       </div>
     </div>
   );
